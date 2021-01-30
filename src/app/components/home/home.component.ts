b5dd3d0dcd46edb79c9a531f9b5e42b966dd378a
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-01-29 22:18:11 -0300

    Eliminamos los archivos CSS

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,6 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   newReleases: any[] = [];
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,7 +5,6 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
   artists: any[] = [];
